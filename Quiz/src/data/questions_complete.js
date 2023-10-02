1efdfb5c3f58b8e4d6dd55bc8bfb9426316e2662
Jasson Júnior <99465676+JassonJr1@users.noreply.github.com>
2023-10-01 23:43:40 -0400

    Add files via upload

---
+++ Quiz/src/data/questions_complete.js
@@ -0,0 +1,93 @@
+const data = [
+  {
+    category: "HTML",
+    questions: [
+      {
+        question: "Identifique o elemento HTML presente somente em HTML5",
+        options: ["<figure>", "<frame>", "<iframe>", "<legend>"],
+        answer: "<figure>",
+        tip: "É um elemento semantico",
+      },
+      {
+        question: "Em um documento HTML, o lugar correto para se declarar folhas de estilo CSS externas é dentro da seção:",
+        options: ["<div>", "<head>", "<body>", "<title>"],
+        answer: "<head>",
+        tip: "É o mesmo elemnto usado para identificar a cabeça do documento HTML",
+      },
+      {
+        question: "As listas não ordenadas tem a tag de:",
+        options: ["<ol>", "<ul>", "<li>", "<list>"],
+        answer: "<ul>",
+      },
+      {
+        question: "Qual atributo deixa o input obrigatório?",
+        options: ["placeholder", "value", "required", "maxlength"],
+        answer: "required",
+      },
+      {
+        question: "A tag semântica indicada para rodapés é a:",
+        options: ["div", "main", "section", "footer"],
+        answer: "footer",
+      },
+    ],
+  },
+  {
+    category: "CSS",
+    questions: [
+      {
+        question: "Qual regra altera a cor de um elemento?",
+        options: ["color", "background-color", "font-size", "transition"],
+        answer: "color",
+        tip: "Cor em inglês",
+      },
+      {
+        question: "Para aumentar a fonte de um elemento utilizamos:",
+        options: ["font", "text-transform", "font-size", "hover"],
+        answer: "font-size",
+      },
+      {
+        question: "A posição que deixa um elemento fixo é a:",
+        options: ["static", "absolute", "fixed", "relative"],
+        answer: "fixed",
+      },
+    ],
+  },
+  {
+    category: "JavaScript",
+    questions: [
+      {
+        question: "O que é Vanilla JavaScript?",
+        options: [
+          "JavaScript puro",
+          "Uma biblioteca JavaScript",
+          "Um framework JavaScript",
+          "Um compilador de JavaScript",
+        ],
+        answer: "JavaScript puro",
+      },
+      {
+        question: "Com qual instrução declaramos uma constante em JavaScript?",
+        options: ["const", "let", "var", "define"],
+        answer: "const",
+      },
+      {
+        question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
+        options: ["string", "number", "boolean", "float"],
+        answer: "float",
+      },
+      {
+        question: "Qual dos métodos a seguir seleciona um elemento?",
+        options: ["querySelector", "parseInt", "sort", "reduce"],
+        answer: "querySelector",
+      },
+      {
+        question:
+          "Qual destas propriedades da a quantidade de elementos de um array?",
+        options: ["qty", "length", "items", "index"],
+        answer: "length",
+      },
+    ],
+  },
+];
+
+export default data;
